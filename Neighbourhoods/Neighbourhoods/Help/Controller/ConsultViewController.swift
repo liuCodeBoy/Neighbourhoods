a696c86a490b1556790ddf0fc3df1b8b9107f107
Weslie <iweslie@vip.qq.com>
2017-10-28 17:48:58 +0800

    perfect the neighbour help

---
+++ Neighbourhoods/Neighbourhoods/Help/Controller/ConsultViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ConsultViewController.swift
+//  Neighbourhoods
+//
+//  Created by Weslie on 28/10/2017.
+//  Copyright © 2017 NJQL. All rights reserved.
+//
+
+import UIKit
+
+class ConsultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setNavBarTitle(title: "南京某公益组织")
+        setNavBarBackBtn()
+
+    }
+
+
+
+}
--- Neighbourhoods/Neighbourhoods/Profile/Controller/EditProfileViewController.swift
+++ Neighbourhoods/Neighbourhoods/Profile/Controller/EditProfileViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class EditProfileViewController: UIViewController {
     
+    
+    
     @IBOutlet weak var editTableVIew: UITableView!
     
     override func viewDidLoad() {
