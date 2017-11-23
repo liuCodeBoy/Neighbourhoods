7130c1989bb1f2bc52bdf18e6290ef31ba7f3720
Weslie <iweslie@vip.qq.com>
2017-11-23 21:02:56 +0800

    complete register and change app icon

--- Neighbourhoods/Neighbourhoods/Profile/Controller/InitialLogin/RegisterViewController.swift
+++ Neighbourhoods/Neighbourhoods/Profile/Controller/InitialLogin/RegisterViewController.swift
@@ -104,41 +104,52 @@ class RegisterViewController: UIViewController {
         } else {
             if phoneNumber.text!.isValidePhoneNumber == true {
                 
-//                SMSSDK.commitVerificationCode(idNumber.text!, phoneNumber: phoneNumber.text!, zone: "86", result: { (error: Error?) in
-//                    if error != nil {
-//                        self.presentHintMessage(target: self, hintMessgae: "验证码输入错误")
-//                    } else {
-//                        weak var weakSelf = self
-//                        NetWorkTool.shareInstance.UserRegister((weakSelf?.phoneNumber.text!)!, password: (weakSelf?.password.text!)!, finished: { [weak self](userInfo, error) in
-//                            if error == nil {
-//                                //存储数据到服务器
-//                                let alert = UIAlertController(title: "提示", message: "注册成功", preferredStyle: .alert)
-//                                let ok = UIAlertAction(title: "好的", style: .default, handler: { (_) in
-//                                    //登陆界面销毁
-//                                    weakSelf?.navigationController?.popToRootViewController(animated: true)
-//                                })
-//                                alert.addAction(ok)
-//                                weakSelf?.present(alert, animated: true, completion: nil)
-//
-//                            }
-//                        })
-//                    }
-//                })
-                
-                presentHintMessage(hintMessgae: "success", completion: { (_) in
-                    
-                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Welcome")
-                    self.present(vc!, animated: true, completion: {
-                       
-                    })
+                SMSSDK.commitVerificationCode(idNumber.text!, phoneNumber: phoneNumber.text!, zone: "86", result: { (error: Error?) in
+                    if error != nil {
+                        self.presentHintMessage(hintMessgae: "验证码输入错误", completion: nil)
+                    } else {
+                        weak var weakSelf = self
+                        NetWorkTool.shareInstance.UserRegister((weakSelf?.phoneNumber.text!)!, password: (weakSelf?.password.text!)!, finished: { [weak self](userInfo, error) in
+                            if error == nil {
+                                // MARK:- judge the return data from server
+                                if userInfo!["code"] as! String == "200" {
+                                    // MARK:- save token to user dafaults
+                                    let token = userInfo!["result"]!["token"]
+                                    UserDefaults.standard.setValue(token!, forKey: "token")
+                                    
+                                    let alert = UIAlertController(title: "提示", message: "注册成功", preferredStyle: .alert)
+                                    let ok = UIAlertAction(title: "好的", style: .default, handler: { (_) in
+                                        // MARK:- show main interface
+                                        let vc = self?.storyboard?.instantiateViewController(withIdentifier: "Welcome")
+                                        self?.present(vc!, animated: true, completion: {
+                                        })
+                                    })
+                                    alert.addAction(ok)
+                                    weakSelf?.present(alert, animated: true, completion: nil)
+                                    
+                                } else if userInfo!["code"] as! String == "400" {
+                                    self?.presentHintMessage(hintMessgae: "该账号已注册", completion: nil)
+                                } else if userInfo!["code"] as! String == "401" {
+                                    self?.presentHintMessage(hintMessgae: "注册失败", completion: nil)
+                                } else if userInfo!["code"] as! String == "415" {
+                                    self?.presentHintMessage(hintMessgae: "错误的请求类型", completion: nil)
+                                } else {
+                                    print("post request failed with exit code \(userInfo!["code"] as! String)")
+                                }
+                                
+                                
+                                
+
+                            } else {
+                                print(error as AnyObject)
+                            }
+                        })
+                    }
                 })
             } else {
                 self.presentHintMessage(hintMessgae: "请输入正确的手机号码", completion: nil)
-            
-     
             }
         }
-        
         
     }
     
@@ -147,6 +158,4 @@ class RegisterViewController: UIViewController {
     }
 
 }
-
-
 
