0fe845f4edac916dd26a115ea49952b2427820d3
Weslie <iweslie@vip.qq.com>
2017-12-04 16:03:37 +0800

    final edition

--- Neighbourhoods/Neighbourhoods/Profile/Controller/InitialLogin/RegisterViewController.swift
+++ Neighbourhoods/Neighbourhoods/Profile/Controller/InitialLogin/RegisterViewController.swift
@@ -72,6 +72,17 @@ class RegisterViewController: UIViewController {
         let back = UIBarButtonItem(image: #imageLiteral(resourceName: "back"), style: .done, target: self, action: #selector(pop))
         self.navBar.setLeftBarButton(back, animated: true)
     }
+    
+    @IBOutlet weak var checkBoxBtn: UIButton!
+    @IBAction func chechBoxClicked(_ sender: UIButton) {
+        checkBoxBtn.isSelected = !checkBoxBtn.isSelected
+        
+        if checkBoxBtn.isSelected == false {
+            
+        }
+    }
+    
+    
     @IBAction func sendIDNumAction(_ sender: Any) {
         if phoneNumber.text == "" {
             self.presentHintMessage(hintMessgae: "请输入手机号码", completion: nil)
@@ -92,7 +103,6 @@ class RegisterViewController: UIViewController {
     @IBAction func registeAction(_ sender: Any) {
         if phoneNumber.text == "" {
             self.presentHintMessage(hintMessgae: "请输入手机号码", completion: nil)
-            
         } else if phoneNumber.text?.isValidePhoneNumber == false {
             self.presentHintMessage(hintMessgae: "请输入正确的手机号码", completion: nil)
         } else if password.text?.isValidPassword == false {
@@ -101,63 +111,37 @@ class RegisterViewController: UIViewController {
             self.presentHintMessage(hintMessgae: "请输入密码", completion: nil)
         } else if idNumber.text == "" {
             self.presentHintMessage(hintMessgae: "请输入验证码", completion: nil)
-        } else {
-            if phoneNumber.text!.isValidePhoneNumber == true {
-                
-                SMSSDK.commitVerificationCode(idNumber.text!, phoneNumber: phoneNumber.text!, zone: "86", result: { (error: Error?) in
-                    if error != nil {
-                        self.presentHintMessage(hintMessgae: "验证码输入错误", completion: nil)
-                    } else {
-                        weak var weakSelf = self
-                        NetWorkTool.shareInstance.UserRegister((weakSelf?.phoneNumber.text!)!, password: (weakSelf?.password.text!)!, finished: { [weak self](userInfo, error) in
-                            if error == nil {
-                                // MARK:- judge the return data from server
-                                if userInfo!["code"] as! String == "200" {
-                                    // MARK:- save token to user dafaults
-                                    let token = userInfo!["result"]!["token"]
-                                    UserDefaults.standard.setValue(token!, forKey: "token")
-                                    let uid = userInfo!["result"]!["uid"]
-                                    UserDefaults.standard.setValue(uid!, forKey: "uid")
-                                    UserDefaults.standard.synchronize()
-                                    
-                                    let alert = UIAlertController(title: "提示", message: "注册成功", preferredStyle: .alert)
-                                    let ok = UIAlertAction(title: "好的", style: .default, handler: { (_) in
-                                        // MARK:- show main interface
-                                        let vc = self?.storyboard?.instantiateViewController(withIdentifier: "Welcome")
-                                        self?.present(vc!, animated: true, completion: {
-                                        })
-                                    })
-                                    alert.addAction(ok)
-                                    weakSelf?.present(alert, animated: true, completion: nil)
-                                    
-                                } else if userInfo!["code"] as! String == "400" {
-                                    self?.presentHintMessage(hintMessgae: "该账号已注册", completion: nil)
-                                } else if userInfo!["code"] as! String == "401" {
-                                    self?.presentHintMessage(hintMessgae: "注册失败", completion: nil)
-                                } else if userInfo!["code"] as! String == "415" {
-                                    self?.presentHintMessage(hintMessgae: "错误的请求类型", completion: nil)
-                                } else {
-                                    //print("post request failed with exit code \(userInfo!["code"] as! String)")
-                                }
-                                
-                                
-                                
-
-                            } else {
-                                //print(error as AnyObject)
-                            }
-                        })
-                    }
-                })
-            } else {
-                self.presentHintMessage(hintMessgae: "请输入正确的手机号码", completion: nil)
-            }
         }
-        
     }
     
     @objc func pop() {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if phoneNumber.text == "" {
+            self.presentHintMessage(hintMessgae: "请输入手机号码", completion: { (_) in
+                return
+            })
+        } else if phoneNumber.text?.isValidePhoneNumber == false {
+            self.presentHintMessage(hintMessgae: "请输入正确的手机号码", completion: { (_) in
+                return
+            })
+        } else if password.text?.isValidPassword == false {
+            self.presentHintMessage(hintMessgae: "密码应为6-20位字母和数字组合", completion: nil)
+        } else if password.text == "" {
+            self.presentHintMessage(hintMessgae: "请输入密码", completion: { (_) in
+                return
+            })
+        } else if idNumber.text == "" {
+            self.presentHintMessage(hintMessgae: "请输入验证码", completion: { (_) in
+                return
+            })
+        }
+        let dest = segue.destination as! ProtocalsViewController
+        dest.phoneNumber = phoneNumber.text!
+        dest.password = password.text!
+        dest.idNumber = idNumber.text!
     }
 
 }
